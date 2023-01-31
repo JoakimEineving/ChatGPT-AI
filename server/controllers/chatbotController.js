a681686678c3848fce638a0d4d7e7c3dfb7bcd2b
joakimeineving <Joakim.eineving@gmail.com>
2023-01-31 20:40:09 +0100

    chatbot controller

--- server/controllers/chatbotController.js
+++ server/controllers/chatbotController.js
@@ -7,23 +7,28 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-export default class ChatbotController {
-  static async generateResponse(model, prompt, temperature) {
-    try {
-      const response = await openai.createCompletion({
-        model: model,
-        prompt: `${prompt}`,
-        temperature: temperature,
-        max_tokens: 3000,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-      });
-      return response.data.choices[0].text;
-    } catch (error) {
-      console.log(error);
-      throw error;
+const chatbot = async (req, res) => {
+  try {
+    const model = req.body.model;
+    const prompt = req.body.prompt;
+    const temperature = parseFloat(req.body.temperature);
+    console.log(temperature);
+    console.log(req.body);
+    const response = await openai.createCompletion({
+      model: model,
+      prompt: `${prompt}`,
+      temperature: temperature,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0
     }
+    );
+    res.status(200).send({ bot: response.data.choices[0].text });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error || "Something went wrong");
   }
 }
+export default chatbot;
 
--- server/routes/chatbotRoute.js
+++ server/routes/chatbotRoute.js
@@ -1,18 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import ChatbotController from "../controllers/chatbotController.js";
+import chatbot from "../controllers/chatbotController.js";
 
-router.post("/", async (req, res) => {
-    try {
-      const model = req.body.model;
-      const prompt = req.body.prompt;
-      const temperature = parseFloat(req.body.temperature);
-      console.log(temperature);
-      const botResponse = await ChatbotController.generateResponse(model, prompt, temperature);
-      res.status(200).send({ bot: botResponse });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send(error || "Something went wrong");
-    }
-});
+router.post("/", chatbot);
 export default router;
